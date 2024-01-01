661c8155890428140a8712a82c3b839ed186658d
paikwiki <paikwiki@gmail.com>
2024-01-01 23:41:32 +0900

    :sparkles: Show list by each h1 text

---
+++ paik_cli/config.py
@@ -0,0 +1,3 @@
+class AppConfig:
+    def __init__(self, memo_folder_path):
+        self.memo_folder_path = memo_folder_path
--- paik_cli/main.py
+++ paik_cli/main.py
@@ -2,19 +2,37 @@ import json
 import inquirer
 from paik2json.line_manager import LineManager
 from paik2json.parser import Parser
+from config import AppConfig
+
+config = AppConfig(memo_folder_path="sample")
 
 
 def run():
-    questions = [
-        inquirer.Text("file_path", message="File path", default="sample/memo.paik")
-    ]
-    answers = inquirer.prompt(questions)
-
-    with open(answers["file_path"], "r") as f:
+    file_path = f"{config.memo_folder_path}/memo-202401.paik"
+    with open(file_path, "r") as f:
         greeting = f.read()
 
     parser = Parser(LineManager(greeting))
-    print(json.dumps(parser.parse(), indent=2, ensure_ascii=False))
+    memo = parser.parse()
+
+    choices = [title for title in memo.keys()]
+    questions = [
+        inquirer.List(
+            "h1_title",
+            message="select 1st heading",
+            choices=["All"] + choices,
+        ),
+    ]
+
+    answers = inquirer.prompt(questions)
+    h1_title = answers["h1_title"]
+
+    if h1_title == "All":
+        data = memo
+    else:
+        data = memo[h1_title]
+
+    print(json.dumps(data, indent=2, ensure_ascii=False))
 
 
 if __name__ == "__main__":
